5b2582c3c142a97919557f66478220f1afae3ed1
krunalrami111 <83574039+krunalrami111@users.noreply.github.com>
2022-04-24 23:56:59 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,15 +1,15 @@
 
 variable "sns_name" {
         description = "Name of the SNS Topic to be created"
-        default = "my_first_sns"
+        default = "infosns"
 }
 
 variable "account_id" {
         description = "My Accout Number"
-        default = "336577180241"
+        default = "<AWSAccountno>"
 }
 
 variable "keyname" {
         description = "name of exeisitng key pair"
-        default = "krunalrami"
+        default = "<keypairname>"
 }
